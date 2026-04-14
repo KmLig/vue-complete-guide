c87e009b15a0b389eb34478355109cf242927898
Khiem <phamxuankhiem1102@gmail.com>
2026-04-14 23:50:10 +0700

    Add name input handling in app.js and update index.html to display user input

--- basics-03-events-starting-code/basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/basics-03-events-starting-code/app.js
@@ -2,9 +2,13 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
     };
   },
   methods: {
+    setName(event) {
+      this.name = event.target.value;
+    },    
     add(num, event) {
       this.counter += num;
       console.log(this.counter, event);  
