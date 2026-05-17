e06f17b5f421e950c921db1604c0e808b94f6b91
Khiem <phamxuankhiem1102@gmail.com>
2026-05-17 22:44:59 +0700

    Implement user input handling in app.js and update index.html to display real-time input and confirmed input on Enter key press.

--- basics-assignment-2-problem/app.js
+++ basics-assignment-2-problem/app.js
@@ -1,12 +1,21 @@
 const app = Vue.createApp({
     data() {
         return{
-
+            userInput: '',
+            confirmedInput: '',
         }
     },
     methods: {
         showAlert() {
             alert('Hello, Vue!');
+        },
+        registerInput(event) {
+            this.userInput = event.target.value;
+        },
+        registerInputEnter(event) {
+            if (event.key === 'Enter') {
+                this.confirmedInput = event.target.value;
+            }
         }
     }
 }).mount('#assignment');
